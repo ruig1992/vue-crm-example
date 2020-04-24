2b988efbc529f2d08f900e772d8c325fabb37965
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-04-24 22:06:35 +0300

    Add materialize-css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <button class="btn">Test</button>
     <router-view/>
   </div>
 </template>
+
+<style>
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/css/index.css';
+</style>
