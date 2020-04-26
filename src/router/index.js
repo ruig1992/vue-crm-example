2253b05858cf3c848c090a0bde61f496e879c577
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-04-26 13:32:45 +0300

    Add Categories

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,14 @@ const routes = [
     name: 'Login',
     component: () => import('../views/Login.vue'),
   },
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: () => import('../views/Categories.vue'),
+    meta: {
+      ...metaDefaultLayout,
+    },
+  },
   // route level code-splitting
   // this generates a separate chunk (about.[hash].js) for this route
   // which is lazy-loaded when the route is visited.
---
+++ src/views/Categories.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="page-title">
+      <h3>Категорії</h3>
+    </div>
+    <section>
+      <div class="row">
+        <div class="col s12 m6">
+          <div>
+            <div class="page-subtitle">
+              <h4>Створити</h4>
+            </div>
+
+            <form>
+              <div class="input-field">
+                <input
+                    id="name"
+                    type="text"
+                >
+                <label for="name">Назва</label>
+                <span class="helper-text invalid">Введіть назву</span>
+              </div>
+
+              <div class="input-field">
+                <input
+                    id="limit"
+                    type="number"
+                >
+                <label for="limit">Ліміт</label>
+                <span class="helper-text invalid">Мінімальна величина</span>
+              </div>
+
+              <button class="btn waves-effect waves-light" type="submit">
+                Створити
+                <i class="material-icons right">send</i>
+              </button>
+            </form>
+          </div>
+        </div>
+        <div class="col s12 m6">
+          <div>
+            <div class="page-subtitle">
+              <h4>Редагувати</h4>
+            </div>
+
+            <form>
+              <div class="input-field">
+                <select>
+                  <option>Category</option>
+                </select>
+                <label>Виберіть категорію</label>
+              </div>
+
+              <div class="input-field">
+                <input type="text" id="name">
+                <label for="name">Назва</label>
+                <span class="helper-text invalid">TITLE</span>
+              </div>
+
+              <div class="input-field">
+                <input
+                    id="limit"
+                    type="number"
+                >
+                <label for="limit">Ліміт</label>
+                <span class="helper-text invalid">LIMIT</span>
+              </div>
+
+              <button class="btn waves-effect waves-light" type="submit">
+                Оновити
+                <i class="material-icons right">send</i>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Categories',
+};
+</script>
