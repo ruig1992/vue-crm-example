083f32bb15db5cfaa5acaed44b42dd7a4daca1e3
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-04-26 19:58:24 +0300

    Add validation to login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,40 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="onSubmit">
     <div class="card-content">
       <span class="card-title">Домашня бухгалтерія</span>
       <div class="input-field">
         <input
-            id="email"
-            type="text"
-            class="validate"
+          id="email"
+          type="text"
+          v-model.trim="$v.email.$model"
+          :class="{ invalid: $v.email.$error }"
         >
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+          class="helper-text invalid"
+          v-if="$v.email.$error && !$v.email.required"
+        >Email is required</small>
+        <small
+          class="helper-text invalid"
+          v-if="$v.email.$error && !$v.email.email"
+        >Email is invalid format</small>
       </div>
       <div class="input-field">
         <input
-            id="password"
-            type="password"
-            class="validate"
+          id="password"
+          type="password"
+          v-model.trim="$v.password.$model"
+          :class="{ invalid: $v.password.$error }"
         >
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          class="helper-text invalid"
+          v-if="$v.password.$error && !$v.password.required"
+        >Password is required</small>
+        <small
+          class="helper-text invalid"
+          v-if="$v.password.$error && !$v.password.minLength"
+        >Password must have at least {{ $v.password.$params.minLength.min }} characters</small>
       </div>
     </div>
     <div class="card-action">
@@ -41,7 +57,41 @@
 </template>
 
 <script>
+import { required, email, minLength } from 'vuelidate/lib/validators';
+
 export default {
   name: 'Login',
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+  validations: {
+    email: {
+      required,
+      email,
+    },
+    password: {
+      required,
+      minLength: minLength(4),
+    },
+  },
+  methods: {
+    onSubmit() {
+      this.$v.$touch();
+
+      if (this.$v.$invalid) {
+        console.error('Form is invalid!');
+        return;
+      }
+
+      const formData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      console.log(formData);
+      this.$router.push('/');
+    },
+  },
 };
 </script>
