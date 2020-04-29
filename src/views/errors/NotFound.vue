6d80b599e6512431f4eadf54b84c462159a7c780
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-04-29 20:03:16 +0300

    Add NotFound - 404 err view

---
+++ src/views/errors/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card blue-grey darken-1">
+    <div class="card-content white-text">
+      <span class="card-title">404 - Сторінку не знайдено!</span>
+      <p>Сторінку за цією адресою не знайдено...</p>
+    </div>
+    <div class="card-action">
+      Перейдіть на -
+      <router-link :to="{ name: 'Home' }">головну сторінку</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
