f7cf5f0b440f9a68260d188abe5be57ee21e2e5a
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-05-01 16:24:00 +0300

    Set username input after reload the page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -7,25 +7,25 @@
     <form class="form" @submit.prevent="onSubmit">
       <div class="input-field">
         <input
-          id="username"
+          id="name"
           type="text"
-          v-model.trim="$v.username.$model"
-          :class="{ invalid: $v.username.$error }"
+          v-model.trim="$v.name.$model"
+          :class="{ invalid: $v.name.$error }"
         >
-        <label for="username" :class="{ 'active': username }">Ім'я</label>
+        <label for="name" :class="{ 'active': name }">Ім'я</label>
         <small
           class="helper-text invalid"
-          v-if="$v.username.$error && !$v.username.required"
-        >Username is required</small>
+          v-if="$v.name.$error && !$v.name.required"
+        >name is required</small>
         <small
           class="helper-text invalid"
-          v-if="$v.username.$error && !$v.username.minLength"
-        >Username must have at least {{ $v.username.$params.minLength.min }} characters</small>
+          v-if="$v.name.$error && !$v.name.minLength"
+        >name must have at least {{ $v.name.$params.minLength.min }} characters</small>
 
         <small
           class="helper-text invalid"
-          v-if="$v.username.$error && $v.username.required && !$v.username.alphaNumExtra"
-        >Username accepts only letters, numbers with spaces</small>
+          v-if="$v.name.$error && $v.name.required && !$v.name.alphaNumExtra"
+        >name accepts only letters, numbers with spaces</small>
       </div>
 
       <button
@@ -49,10 +49,14 @@ export default {
   name: 'Profile',
   data() {
     return {
-      username: this.$store.getters.user.info.name,
+      name: this.$store.getters.user.info.name,
+      isFirstSetting: true,
     };
   },
   computed: {
+    userName() {
+      return this.$store.getters.user.info.name;
+    },
     isLoading() {
       return this.$store.getters.loading;
     },
@@ -60,18 +64,28 @@ export default {
       return this.$store.getters.error;
     },
   },
+  watch: {
+    userName(val) {
+      if (this.isFirstSetting) {
+        this.name = val;
+        this.isFirstSetting = false;
+      }
+    },
+  },
   validations: {
-    username: { required, minLength: minLength(4), alphaNumExtra },
+    name: { required, minLength: minLength(4), alphaNumExtra },
   },
   methods: {
     async onSubmit() {
+      this.isFirstSetting = false;
+
       this.$v.$touch();
       if (this.$v.$invalid) {
         return;
       }
 
       await this.$store.dispatch('updateProfile', {
-        username: this.username,
+        username: this.name,
       });
 
       /* if (this.error.status) {
