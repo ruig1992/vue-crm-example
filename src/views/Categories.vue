07925856effd1836b8b2cebc6cef237c78bec598
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-05-05 14:09:08 +0300

    Add CategoryEdit component

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -9,40 +9,7 @@
           <CategoryCreate />
         </div>
         <div class="col s12 m6">
-          <div>
-            <div class="page-subtitle">
-              <h4>Редагувати</h4>
-            </div>
-
-            <form>
-              <div class="input-field">
-                <select>
-                  <option>Category</option>
-                </select>
-                <label>Виберіть категорію</label>
-              </div>
-
-              <div class="input-field">
-                <input type="text" id="name">
-                <label for="name">Назва</label>
-                <span class="helper-text invalid">TITLE</span>
-              </div>
-
-              <div class="input-field">
-                <input
-                    id="limit"
-                    type="number"
-                >
-                <label for="limit">Ліміт</label>
-                <span class="helper-text invalid">LIMIT</span>
-              </div>
-
-              <button class="btn waves-effect waves-light" type="submit">
-                Оновити
-                <i class="material-icons right">send</i>
-              </button>
-            </form>
-          </div>
+          <CategoryEdit />
         </div>
       </div>
     </section>
@@ -51,9 +18,10 @@
 
 <script>
 import CategoryCreate from '@/components/Category/CategoryCreate.vue';
+import CategoryEdit from '@/components/Category/CategoryEdit.vue';
 
 export default {
   name: 'Categories',
-  components: { CategoryCreate },
+  components: { CategoryCreate, CategoryEdit },
 };
 </script>
