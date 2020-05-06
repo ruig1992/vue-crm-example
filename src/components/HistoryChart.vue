bd0f7abb353a931dfa510053ab1c5f275d2c0116
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-05-06 23:02:28 +0300

    Add chart

---
+++ src/components/HistoryChart.vue
@@ -0,0 +1,15 @@
+<script>
+import { Pie } from 'vue-chartjs';
+
+export default {
+  name: 'HistoryChart',
+  extends: Pie,
+  props: {
+    data: { type: Object, default: null },
+    options: { type: Object, default: null },
+  },
+  mounted() {
+    this.renderChart(this.data, this.options);
+  },
+};
+</script>
--- src/views/History.vue
+++ src/views/History.vue
@@ -4,8 +4,8 @@
       <h3>Історія записів</h3>
     </div>
 
-    <div class="history-chart">
-      <canvas></canvas>
+    <div class="history-chart" v-if="!loading">
+      <HistoryChart :data="chartData" :options="chartOptions" />
     </div>
 
     <CircularLoader v-if="loading" />
@@ -37,34 +37,77 @@
 </template>
 
 <script>
+import { Pie } from 'vue-chartjs';
 import paginationMixin from '@/mixins/pagination.mixin';
+import HistoryChart from '@/components/HistoryChart.vue';
 import HistoryTable from '@/components/HistoryTable.vue';
-import { TYPE_INCOME } from '@/services/record.service';
+import { TYPE_OUTCOME } from '@/services/record.service';
 
 export default {
   name: 'History',
-  components: { HistoryTable },
+  components: { HistoryChart, HistoryTable },
   mixins: [paginationMixin],
+  extends: Pie,
   data: () => ({
     records: [],
     loading: true,
+    chartData: null,
+    chartOptions: null,
   }),
   async mounted() {
     const categories = await this.$store.dispatch('getCategories');
     this.records = await this.$store.dispatch('getRecords');
 
-    this.setupPagination(this.records.map((r) => {
-      const categoryTitle = categories.find((c) => c.id === r.category_id)
-        .title;
-      const typeText = r.type === TYPE_INCOME ? 'Дохід' : 'Витрата';
-      const typeClass = r.type === TYPE_INCOME ? 'green' : 'red';
-
-      return {
-        ...r, categoryTitle, typeText, typeClass,
-      };
-    }));
-
+    this.setup(categories);
     this.loading = false;
+  },
+  methods: {
+    setup(categories) {
+      this.chartData = {
+        labels: categories.map((c) => c.title),
+        datasets: [{
+          label: 'Витрати за категоріями',
+          /* eslint-disable */
+          data: categories.map((c) => this.records
+            .reduce((sum, r) => {
+              if (r.category_id === c.id && r.type === TYPE_OUTCOME) {
+                sum += +r.amount;
+              }
+              return sum;
+            }, 0),
+          ),
+          /* eslint-enable */
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        }],
+      };
+
+      this.setupPagination(this.records.map((r) => {
+        const categoryTitle = categories.find((c) => c.id === r.category_id)
+          .title;
+        const typeText = r.type === TYPE_OUTCOME ? 'Витрата' : 'Дохід';
+        const typeClass = r.type === TYPE_OUTCOME ? 'red' : 'green';
+
+        return {
+          ...r, categoryTitle, typeText, typeClass,
+        };
+      }));
+    },
   },
 };
 </script>
