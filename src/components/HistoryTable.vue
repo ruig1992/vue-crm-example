8790274b2888c0e2c3a13726d21c79b34041c6da
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-05-06 18:24:52 +0300

    Add HistoryTable

---
+++ src/components/HistoryTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Сума</th>
+          <th>Дата</th>
+          <th>Категорія</th>
+          <th>Тип</th>
+          <th>Відкрити</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="(r, i) in records" :key="r.id">
+          <td>{{ i + 1 }}</td>
+          <td>{{ r.amount | currency }}</td>
+          <td>{{ r.date | date('shortdatetime') }}</td>
+          <td>{{ r.categoryTitle }}</td>
+          <td>
+            <span class="white-text badge"
+              :class="r.typeClass"
+            >{{ r.typeText }}</span>
+          </td>
+          <td>
+            <router-link
+              :to="{ name: 'DetailRecord', params: { id: r.id } }"
+              class="btn-small btn"
+            >
+              <i class="material-icons">open_in_new</i>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HistoryTable',
+  props: {
+    records: { type: Array, required: true },
+  },
+};
+</script>
--- src/views/History.vue
+++ src/views/History.vue
@@ -15,50 +15,17 @@
       <router-link :to="{ name: 'NewRecord' }">прямо зараз</router-link>
     </p>
 
-    <section v-else>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Сума</th>
-            <th>Дата</th>
-            <th>Категорія</th>
-            <th>Тип</th>
-            <th>Відкрити</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr v-for="(r, i) in records" :key="r.id">
-            <td>{{ i + 1 }}</td>
-            <td>{{ r.amount | currency }}</td>
-            <td>{{ r.date | date('shortdatetime') }}</td>
-            <td>{{ r.categoryTitle }}</td>
-            <td>
-              <span class="white-text badge"
-                :class="r.typeClass"
-              >{{ r.typeText }}</span>
-            </td>
-            <td>
-              <router-link
-                :to="{ name: 'DetailRecord', params: { id: r.id } }"
-                class="btn-small btn"
-              >
-                <i class="material-icons">open_in_new</i>
-              </router-link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
+    <HistoryTable :records="records" v-else />
   </div>
 </template>
 
 <script>
+import HistoryTable from '@/components/HistoryTable.vue';
 import { TYPE_INCOME } from '@/services/record.service';
 
 export default {
   name: 'History',
+  components: { HistoryTable },
   data: () => ({
     records: [],
     loading: true,
